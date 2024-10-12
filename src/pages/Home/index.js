964113ed990ee4a439119ba34889ae32e929b76e
Matheus Souza <szmatheussouza@gmail.com>
2024-10-12 20:03:53 -0300

    implementando api do the movie-db

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+// URL: https://api.themoviedb.org/3/movie/now_playing?api_key=88b6db293bfea153a6257eddbeddd548&language=pt-BR
 
 function Home(){
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=> {
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "88b6db293bfea153a6257eddbeddd548",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+
+      console.log(response);
+    }
+
+      loadFilmes();
+  }, [])
+
   return(
     <div>
       <h1>BEM VINDO A HOME</h1>
