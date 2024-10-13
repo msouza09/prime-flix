e0598fd10002651b6d00d99540b9e7fa2d5fc7b0
Matheus <szmatheussouza@gmail.com>
2024-10-13 13:18:42 -0300

    funcao de listagem e exclusão

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+
+function Favoritos() {
+
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(()=>{
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || [])
+
+    }, [])
+
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter( (filme) => {
+            return (filme.id !== id)
+        })
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes) )
+    }
+
+    return (
+        <div className="meus-filmes">
+            <h1>Meus Filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo.</span>}
+
+        <ul>
+            {filmes.map((filme) => {
+                return (
+                    <li key={filme.id}>
+                        <span>{filme.title}</span>
+                        
+                        <div>
+                            <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                            <button onClick={() => excluirFilme(filme.id)}>Excluir</button>
+                        </div>
+                    </li>
+
+                )
+            })}
+        </ul>
+
+        </div>
+    )
+}
+
+export default Favoritos;
